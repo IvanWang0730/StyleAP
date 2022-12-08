af5504d6522239e3df032be2170d7e69c6172bac
IvanWang0730 <85216690+IvanWang0730@users.noreply.github.com>
2022-12-08 15:47:24 +0800

    Create search_index.sh

---
+++ scripts/search_index.sh
@@ -0,0 +1,9 @@
+FOLDER=$(pwd)
+GPUS=$1
+DATASET=$2
+INDEX=$3
+
+CUDA_VISIBLE_DEVICES=${GPUS} python3 code/faiss.py --mode test \
+ --file_dir ${FOLDER}/MSMT/ \
+ --dataset ${DATASET} --index_path ${INDEX}
+ 
