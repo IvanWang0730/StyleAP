e6682066aaa2bdf1e4881f1b90c25704770068a2
IvanWang0730 <85216690+IvanWang0730@users.noreply.github.com>
2022-12-08 15:46:13 +0800

    Update generate_index.sh

--- scripts/generate_index.sh
+++ scripts/generate_index.sh
@@ -1,10 +1,8 @@
 FOLDER=$(pwd)
 GPUS=$1
-MODE=$2
-TRAIN=$3
-TEST=$4
-INDEX=$5
+DATASET=$2
+INDEX=$3
 
-CUDA_VISIBLE_DEVICES=${GPUS} python3 code/faiss.py --mode ${MODE} \
+CUDA_VISIBLE_DEVICES=${GPUS} python3 code/faiss.py --mode train \
  --file_dir ${FOLDER}/MSMT/ \
- --train ${TRAIN} --test ${TEST} --index_path ${INDEX}
+ --dataset ${DATASET} --index_path ${INDEX}
